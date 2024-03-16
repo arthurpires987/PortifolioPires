e27997a15e04153e4f7ce9422fe630b606326024
Arthur Pires <121984422+arthurpires987@users.noreply.github.com>
2024-03-16 17:51:01 -0300

    novo commit

---
+++ email.php
@@ -0,0 +1,26 @@
+<?php
+
+    if(isset($_POST['email']) && !empty($_POST['email'])) {
+
+    $nome = addslashes($_POST['name']);
+    $email = addslashes($_POST['email']);
+    $mensagem = addslashes($_POST['message']);
+
+    $to = "arthur.piresantunes987@gmail.com";
+    $subject = "Contato do Portifolio";
+    $body = "Nome: ".$nome."\r\n".
+            "Email: ".$email. "\r\n".
+            "Mensagem: ".$mensagem;
+
+    $header = "From: arthurmanu7@gmail.com"."\r\n".
+        "Reply-To:".$email."\r\n".
+        "X=Mailer:PHP/".phpversion();
+    };
+
+    if(mail($to, $subject, $body, $header)) {
+        echo("Email enviado com sucesso!");
+    } else {
+        echo("não foi possivel enviar o email, tente novamente!");
+    }
+
+?>
